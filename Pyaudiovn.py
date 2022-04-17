217dbf892de1144130be69c5b2bdb296c55e6eb8
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-17 22:55:23 +0700

    BTL AI

--- Pyaudiovn.py
+++ Pyaudiovn.py
@@ -1,22 +1,28 @@
 from gtts import gTTS as GT
 import playsound,os,speech_recognition
-
+Folder_path='Sound'
 def Speak_vn(s):
-    pathsound ='Sound\speech.mp3'
+    pathsound =f'{Folder_path}\speech.mp3'
     print("F.R.I.D.A.Y: "+s.title())
     try:
         GT(text=s, lang='vi', slow=False).save(pathsound)
     except:
-        os.mkdir('Sound')
+        os.mkdir(Folder_path)
         GT(text=s, lang='vi', slow=False).save(pathsound)
     playsound.playsound(pathsound,True)
     os.remove(pathsound)
-
+def ReadHitory():
+    path = f'{Folder_path}/history.txt'
+    list=[]
+    with open(path, 'r', encoding='UTF-8') as File:
+        list=File.readlines()
+    return list
+    
 def listen():
     bot = speech_recognition.Recognizer()
     with speech_recognition.Microphone() as mic:
         print("F.R.I.D.A.Y: listening...")
-        # bot.pause_threshold = 1 #dung 2s roi nghe lenh moi
+        bot.pause_threshold = 1 #dung 2s roi nghe lenh moi
         audio = bot.listen(mic)
     s = ""
     try:
@@ -26,7 +32,8 @@ def listen():
     print("You say: "+s)
     return s.lower()
 if __name__=='__main__':
-    Speak_vn('Xin chào2')
-    
+    # Speak_vn('Xin chào2')
+    ReadHitory()
+
 
 
