82d7484dbfa9978e9e9559bed515a12051d4d25f
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-19 11:44:59 +0700

    BTL AI

--- Controler.py
+++ Controler.py
@@ -5,26 +5,23 @@ from AppPC import Applicationcontroler
 
 def AICONTROLER():
     while True:
-        SP,T=ENDAI()
-        if T:
-            if Systemcontroler(SP)!=True:
-                continue
-            elif AppChromeControler(SP)!=True:
-                continue
-            elif Applicationcontroler(SP)!=True:
-                continue
-            else:
-                googlesearch(SP)
-        else :
-            break
-
+        SP=ENDAI()
+        if Systemcontroler(SP)!=True:
+            continue
+        elif AppChromeControler(SP)!=True:
+            continue
+        elif Applicationcontroler(SP)!=True:
+            continue
+        else:
+            googlesearch(SP)
+            
 def ENDAI():
     list=['thôi','tạm biệt','đi','biến ','lướt','thoát','exit','next','goodbye']
     Speak=listen().lower()
-    if Speak!='' and Speak in list:
+    if  Speak in list:
         Speak_vn('Xin chào hẹn gặp lại')
-        return Speak,False
-    return Speak,True
+        raise SystemExit(0)
+    return Speak
 
 if __name__=='__main__':
     AICONTROLER()
